fda48ba307cf47be1de31190ed64c3bfa103f29b
Gohar <gohargevorgyan336@gmail.com>
2026-03-08 00:37:51 +0400

    feat(iot): attach certificate to thing and policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,54 @@ output "s3_bucket_name" {
   value       = aws_s3_bucket.iot_raw_events.bucket
   description = "Name of the S3 bucket for raw IoT events"
 }
+########################################
+# AWS IoT Thing
+########################################
+
+resource "aws_iot_thing" "fire_detector" {
+  name = "fire-detector-device"
+
+  attributes = {
+    project = "iot-hybrid"
+    owner   = "Gohar"
+  }
+}
+########################################
+# IoT Certificate
+########################################
+
+resource "aws_iot_certificate" "fire_detector_cert" {
+  active = true
+}
+########################################
+# IoT Policy
+########################################
+
+resource "aws_iot_policy" "fire_detector_policy" {
+  name = "fire-detector-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iot:Connect",
+          "iot:Publish",
+          "iot:Subscribe",
+          "iot:Receive"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_iot_thing_principal_attachment" "attach_cert_to_thing" {
+  thing     = aws_iot_thing.fire_detector.name
+  principal = aws_iot_certificate.fire_detector_cert.arn
+}
+
+resource "aws_iot_policy_attachment" "attach_policy_to_cert" {
+  policy = aws_iot_policy.fire_detector_policy.name
+  target = aws_iot_certificate.fire_detector_cert.arn
+}
